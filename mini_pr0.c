e653525cfb78d0c84f9c018db0652ef1e7e01fb4
Madhavan T <madhavansaran2003@gmail.com>
2025-12-06 22:46:25 +0530

    Updated mini_pr0.c

--- mini_pr0.c
+++ mini_pr0.c
@@ -5,7 +5,6 @@
 # define sw1 4
 # define sw2 5
 # define sw3 6
-# define sw4 1<<3   \\external interrupt 1
 # define id_limit 10
 # define irq_enable 1<<5
 # define eint1 1<<15
@@ -13,92 +12,99 @@ void uart0_config();
 unsigned char uart0_rx(void);
 void uart0_tx(unsigned char);
 void interrupt_configure(void);
-unsigned char id0[id_limit][50]={{"0600682384C90"},{"060067A552965"},{"060067E061E00"}};
-unsigned char in[]="tap your card";
-unsigned char entry[]="your id is verified";
+unsigned char id0[id_limit][14]={{"062547A95C123"},{"062A47A95C123"},{"762547A95C123"}};
+unsigned char tap[]="tap your card";
+unsigned char verified[]="your id is verified";
 unsigned char one[]="1-BATERY";
 unsigned char two[]="2-VOLT";
 unsigned char three[]="3-BUTTON";
-unsigned char exite[]="welcome";
-unsigned char error[]="you have already scanned";
-unsigned char registered[]="your card not registered";
+unsigned char welcome[]="welcome";
+unsigned char already_scan[]="you have already scanned";
+unsigned char not_registered[]="your card not registered";
+unsigned char again[]="tap your card again for verification";
+unsigned char miss_match[]="card miss match vote not counted";
 unsigned char out1[]="BATERY=";
 unsigned char out2[]="VOLT=";
 unsigned char out3[]="BUTTON=";
+unsigned char spaces[]=" ";
 unsigned int batery=0,volt=0,button=0;
 void tag_value_reader(void);
 void isr1(void) __irq
 {
 	EXTINT=0x02;
+	comand(0x01);
+	comand(0x80);
 	lcd_string(out1);
 	lcd_integer(batery);
+	lcd_string(spaces);
 	lcd_string(out2);
 	lcd_integer(volt);
+	comand(0xc4);
 	lcd_string(out3);
 	lcd_integer(button);
 	VICVectAddr=0;
 }
 int main()
 {
-	unsigned int i=0,j=0;
+	unsigned int i=0,j=0,k=0;
 	unsigned int flag[]={0,0,0};
-	unsigned char a[14];
+	unsigned char a[14],sam[14];
 	init();
 	uart0_config();
 	//uart0_tx('a');
 	//tag_value_reader();
 	PINSEL0|=(3<<6);
 	interrupt_configure();
-	EXTMODE=(1<<1);
-	EXTPOLAR=(0<<1);
+	EXTMODE=0x03;
+	EXTPOLAR=0x00;
 	VICIntEnable=eint1;
 	label:
 	while(1)
 	{
-		j=0;
 		comand(0x01);
 		comand(0x80);
-		lcd_string(in);
+		lcd_string(tap);
 		//delay_milliseconds(300);
 		for(i=0;i<13;i++)
 		{
 			a[i]=uart0_rx();
 		}
 		a[i]='\0';
+		comand(0x01);
+		comand(0x80);
+		lcd_string(a);
+		delay_seconds(2);
 		while(1)
 		{
-			if(flag[j]!=1)
+			if(strcmp(a,id0[j])==0)
 			{
-				if(strcmp(a,id0[j])==0)
+				if(flag[j]==1)
 				{
-					flag[j]=1;
-					break;
-				}	
-				else
-				{
-					j++;
-					if(j==id_limit)
-					{
-						comand(0x01);
-						comand(0x80);
-						lcd_string(registered);
-						//delay_milliseconds(500);
-						goto label;
-					}
+					comand(0x01);
+					comand(0x80);
+					lcd_string(already_scan);
+					delay_seconds(2);
+					goto label;
 				}
-			}
+				flag[j]=1;
+				break;
+			}	
 			else
 			{
-				comand(0x01);
-				comand(0x80);
-				lcd_string(error);
-				delay_seconds(2);
-				goto label;
+				j++;
+				if(j==id_limit)
+				{
+					comand(0x01);
+					comand(0x80);
+					lcd_string(not_registered);
+					delay_milliseconds(2000);
+					goto label;
+				}
 			}
 		}
-comand(0x01);
-comand(0x80);		
-		lcd_string(entry);
+		comand(0x01);
+		comand(0x80);		
+		lcd_string(verified);
 		delay_seconds(2);
 		comand(0x01);
 		comand(0x80);
@@ -112,29 +118,91 @@ comand(0x80);
 			if(((IOPIN0>>sw1)&1)==0)
 			{
 				delay_milliseconds(200);
-				++batery;
 				while(((IOPIN0>>sw1)&1)==0);
+				lcd_string(again);
+				for(k=0;k<13;k++)
+				{
+				sam[k]=uart0_rx();
+				}
+				sam[k]='\0';
+				if(strcmp(a,sam)==0)
+				{
+					++batery;
+					comand(0x01);
+					comand(0x80);
+					lcd_string(welcome);
+				}
+				else
+				{
+					comand(0x01);
+					comand(0x80);
+					flag[j]=0;
+					lcd_string(miss_match);
+					delay_seconds(1);
+				}					
 				break;
 			}
 			else if(((IOPIN0>>sw2)&1)==0)
 			{
 				delay_milliseconds(200);
-				++volt;
 				while(((IOPIN0>>sw2)&1)==0);
+				comand(0x01);
+				comand(0x80);
+				lcd_string(again);
+				for(k=0;k<13;k++)
+				{
+				sam[k]=uart0_rx();
+				}
+				sam[k]='\0';
+				if(strcmp(a,sam)==0)
+				{
+					++volt;
+					comand(0x01);
+					comand(0x80);
+					lcd_string(welcome);
+				}
+				else
+				{
+					comand(0x01);
+					comand(0x80);
+					flag[j]=0;
+					lcd_string(miss_match);
+					delay_seconds(1);
+				}					
 				break;
 			}
 			else if(((IOPIN0>>sw3)&1)==0)
 			{
 				delay_milliseconds(200);
-				++button;
 				while(((IOPIN0>>sw3)&1)==0);
+				comand(0x01);
+				comand(0x80);
+				lcd_string(again);
+				for(k=0;k<13;k++)
+				{
+				sam[k]=uart0_rx();
+				}
+				sam[k]='\0';
+				if(strcmp(a,sam)==0)
+				{
+					++button;
+					comand(0x01);
+					comand(0x80);
+					lcd_string(welcome);
+				}
+				else
+				{
+					comand(0x01);
+					comand(0x80);
+					flag[j]=0;
+					lcd_string(miss_match);
+					delay_seconds(1);
+				}					
 				break;
 			}
 		}
-		comand(0x01);
-		comand(0x80);
-		lcd_string(exite);
 		delay_seconds(2);
+		j=0;
 	}
 }
 void uart0_config(void)
@@ -173,3 +241,4 @@ void interrupt_configure(void)
 	VICVectCntl1=(irq_enable|15);
 	VICVectAddr1=(int)isr1;
 }
+
